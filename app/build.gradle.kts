22259f3b19da637aaf1f7989e89da67f98096802
Ankur Tripathi <ankur_tripathi@hotmail.com>
2024-04-03 12:24:21 +0530

    final commit-ankur

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.anksys.niyointechtask"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.anksys.niyointechtask"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation ("com.android.support:design:28.0.0")
+    implementation ("com.android.support:appcompat-v7:28.0.0")
+    implementation ("com.android.support:support-v4:28.0.0")
+
+}
